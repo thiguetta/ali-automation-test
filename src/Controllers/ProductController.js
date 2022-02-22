80c5d86919d1e6d1d7fef0bf4ffc9a8160f27e50
Thiago Gonzaga <thiago.gonzaga@icloud.com>
2022-02-22 07:22:36 -0300

    add GetProducts API
    Signed-off-by: Thiago Gonzaga <thiago.gonzaga@icloud.com>

--- src/Controllers/ProductController.js
+++ src/Controllers/ProductController.js
@@ -1,5 +1,57 @@
+const webdriver = require('selenium-webdriver');
+const chrome = require("selenium-webdriver/chrome");
+const {By} = require('selenium-webdriver');
+
+const options = new chrome.Options().headless().addArguments("--no-sandbox", "--disable-dev-shm-usage")
+
 exports.getProducts = (req, res, next) => {
-    res.status(201).send('Search result should go here');
+    (async function () {
+        try {
+            let driver = new webdriver.Builder()
+                .forBrowser('chrome')
+                .setChromeOptions(options)
+                .build();
+            if (typeof req.query.page == 'undefined') {
+                req.query.page = 1
+            }
+            await driver.get(`https://www.aliexpress.com/wholesale?SearchText=${req.query.term}&page=${req.query.page}`);
+            let pixelCount = 200;
+            // alternatively we can use By.css("span.total-page") but in this case we opt to use xpath
+            let totalPageElement = await driver.findElements(By.xpath(".//span[@class='total-page']"))
+            // search results page is dynamically loaded, so we need to keep scrolling down until we reach the page bar,
+            // this way we guarantee we have all the listed products for that page
+            // we scroll down 200px at a time
+            while (totalPageElement.length == 0) {
+                await driver.executeScript(`window.scrollTo(0, ${pixelCount})`);
+                pixelCount += 200;
+                totalPageElement = await driver.findElements(By.xpath(".//span[@class='total-page']"))
+            }
+            // span total-page is unique in the page, so we can safely get the first element of the array
+            let totalPages = await totalPageElement[0].getText();
+            let productElements = await driver.findElements(By.css("._3t7zg._2f4Ho"));
+            let selectedPage = await driver.findElement(By.css("button.next-btn.next-medium.next-btn-normal.next-pagination-item.next-current")).getText()
+            let result = {
+                //total pages is in sentence 'Total \d+ pages', so we grab only the number using regex
+                totalPages: parseInt(totalPages.match(/\d+/)[0]),
+                selectedPage: parseInt(selectedPage),
+                products: []
+            };
+            for (let p of productElements) {
+                let url = await p.getAttribute("href");
+                let name = await p.findElement(By.css("h1._18_85")).getText();
+                let id = url.match(/\d+/)[0]
+                result.products.push({
+                    id: id,
+                    name: name,
+                    url: url
+                });
+            }
+            await driver.quit();
+            res.json(result);
+        } catch (e) {
+            console.log(e);
+        }
+    })();
 };
 
 exports.getProductById = (req, res, next) => {
--- test/test.js
+++ test/test.js
@@ -11,10 +11,38 @@ chai.use(chaiHttp);
 describe('Products', () => {
     describe('/GET products for term Iphone', () => {
         it('it should search all the products for the term Iphone and retrieve page 1', (done) => {
-           done();
+            chai.request(server)
+                .get('/products')
+                .query({term: "Iphone"})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('totalPages');
+                    res.body.should.have.property('selectedPage');
+                    res.body.totalPages.should.be.gt(0);
+                    res.body.selectedPage.should.be.eq(1);
+                    res.body.should.have.property('products');
+                    res.body.products.should.be.a('array');
+                    res.body.products.length.should.be.gt(0);
+                    done();
+                });
         });
         it('it should search all the products for the term Iphone and retrieve page 2', (done) => {
-           done();
+            chai.request(server)
+                .get('/products')
+                .query({term: "Iphone", page: 2})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('totalPages');
+                    res.body.should.have.property('selectedPage');
+                    res.body.totalPages.should.be.gt(0);
+                    res.body.selectedPage.should.be.eq(2);
+                    res.body.should.have.property('products');
+                    res.body.products.should.be.a('array');
+                    res.body.products.length.should.be.gt(0);
+                    done();
+                });
         });
         it('it has to get the second ad from the second page of search result for term Iphone', (done) => {
             done();
