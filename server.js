78a3a35302049273ef89c7886cc6dcd05f5c92f1
Thiago Gonzaga <thiago.gonzaga@icloud.com>
2022-02-21 22:35:31 -0300

    initial project skeleton
    Signed-off-by: Thiago Gonzaga <thiago.gonzaga@icloud.com>

---
+++ server.js
@@ -0,0 +1,18 @@
+var process = require('process')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+// start server on port 8080
+app.listen(8080);
+
+// proper handling signals
+process.on('SIGINT', function onSigint() {
+    process.exit(0)
+});
+process.on('SIGTERM', function onSigterm() {
+    process.exit(0)
+});
+
+module.exports = app // for testing
